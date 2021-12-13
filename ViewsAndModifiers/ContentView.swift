a107ce37cf1ab0c419a54eff52f66568d94cefa8
citixenken <kmuyesu@gmail.com>
2021-12-13 21:39:21 +0300

    modifier order

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.red)
+            
+            Button("Hello, world!") {
+                print(type(of: self.body))
+            }
             .background(.red)
+            .frame(width: 200, height: 200)
+            
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+                .padding()
+        }
     }
 }
 
